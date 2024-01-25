4189f17ff39b79c31851835fbe2fa2aca3532f00
Kit Corbita <corbita.khatezz@gmail.com>
2024-01-25 19:05:20 +0800

    Add feature test for crud functionality of Journal

---
+++ app/Http/Controllers/JournalController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\JournalRequest;
+use App\Services\JournalService;
+
+class JournalController extends Controller
+{
+    private $journalService;
+
+    public function __construct(JournalService $journalService)
+    {
+        $this->journalService = $journalService;
+    }
+
+    public function index()
+    {
+        $journals = $this->journalService->doGet();
+        return response(json_encode($journals), 200);
+    }
+    public function store(JournalRequest $request)
+    {
+        $this->journalService->doStore($request->all());
+
+        return response(json_encode('Created'), 201);
+    }
+
+    public function update(Request $request, $journalId)
+    {
+        $this->journalService->doUpdate($request->all(), $journalId);
+        return response(json_encode('Updated'), 200);
+    }
+
+    public function destroy($journalId)
+    {
+        $this->journalService->doDestroy($journalId);
+        return response(json_encode('Deleted'), 204);
+    }
+
+    public function show($journalId)
+    {
+        $journal = $this->journalService->doGetJournal($journalId);
+
+        return response(json_encode($journal), 200);
+    }
+}
