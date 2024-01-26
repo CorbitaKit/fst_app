da2c8867c05b7ec0a2852d506775dce5966b0e89
Kit Corbita <corbita.khatezz@gmail.com>
2024-01-26 14:56:29 +0800

    Add validation test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -25,4 +25,24 @@ abstract class TestCase extends BaseTestCase
     {
         return Journal::factory()->create();
     }
+
+    public function generateDummyDataForJournal(): array
+    {
+        $citizen = $this->generateCitizen();
+        return [
+            'title' => 'Test Title',
+            'content' => 'Sample Content',
+            'date' => '2024-01-26',
+            'citizen_id' => $citizen->id
+        ];
+    }
+
+    public function generateMaximumCharacter(): string
+    {
+        return 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    }
 }
