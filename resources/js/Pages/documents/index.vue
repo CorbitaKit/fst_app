0a24e840704a71e192f3280d247c7fa782eada99
Kit Corbita <corbita.khatezz@gmail.com>
2024-02-26 15:03:06 +0800

    Safe commit

--- resources/js/Pages/documents/index.vue
+++ resources/js/Pages/documents/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import DataTable from 'primevue/datatable';
+    
     import Column from 'primevue/column';
 </script>
 
@@ -9,17 +9,27 @@
         <div class="row">
             <div class="col-md-9">
                 <div class="card">
-                    <DataTable>
-                        <Column field="asset_name" header="Asset Name"></Column>
-                        <Column field="tag" header="Tag"></Column>
-                        <Column field="created_at" header="created"></Column>
+                    <TreeTable  class="`p-treetable-sm">
+                        <template #header>
+                            <div class="flex flex-wrap align-items-center justify-content-between gap-2">
+                                <span class="text-xl text-900 font-bold">Plan List</span>
+                                <Button label="Add new plan"  icon="pi pi-plus" class="float-right" raised />
+                            </div>
+                        </template>
+                        <Column field="name" header="Name" expander></Column>
+                        <Column field="created_at" header="Created" ></Column>
                         <Column field="owner" header="Owner"></Column>
-                        <Column field="updated_at" header="Last Modified"></Column>
-                    </DataTable>
+                        <Column field="updated_at" header="Last Modified" headerStyle="width: 10rem"></Column>
+                        <Column>
+                            <template #body="node">
+                                <div class="flex flex-wrap">
+                                    <Button class="mr-2" @click="getSelectedNode(node)" data-toggle="modal"  data-target="#details" v-tooltip.bottom="'View Details'" icon="pi pi-eye" rounded severity="info" raised />
+                                    <Button type="button" :disabled="node.node.data.status == 'complete'" @click="markAsComplete(node.node.data)" v-tooltip.bottom="'Mark as complete'" icon="pi pi-check" rounded severity="success" raised />
+                                </div>
+                            </template>
+                        </Column>
+                    </TreeTable>
                 </div>
-            </div>
-            <div class="col-md-4">
-
             </div>
         </div>
     </div>
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,17 @@ import './bootstrap';
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'primevue/resources/themes/aura-light-green/theme.css';
 import "primeflex/primeflex.css";
 import 'primeicons/primeicons.css'
 import Tooltip from 'primevue/tooltip';
+import { createPinia } from 'pinia';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Button from 'primevue/button';
+import FileUpload from 'primevue/fileupload';
+import ProgressBar from 'primevue/progressbar';
+import TreeTable from 'primevue/treetable';
 
 createInertiaApp({
   resolve: name => {
@@ -16,7 +23,15 @@ createInertiaApp({
     createApp({ render: () => h(App, props) })
       .use(plugin)
       .use(PrimeVue)
+      .use(createPinia())
       .directive('tooltip', Tooltip)
+      .component('Button', Button)
+      .component('DataTable', DataTable)
+      .component('Column', Column)
+      .component('FileUpload', FileUpload)
+      .component('ProgressBar', ProgressBar)
+      .component('TreeTable', TreeTable)
       .mount(el)
+      
   },
 })
